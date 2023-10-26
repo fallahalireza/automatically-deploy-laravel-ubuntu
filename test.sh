a78fc3d042e905fdf1bba989fc345ef4c970adea
Fallah <falahalireza11@gmail.com>
2023-10-26 20:05:35 +0330

    2 commit

--- test.sh
+++ test.sh
@@ -6,7 +6,8 @@ export $(cat .env_package | xargs)
 echo "name db user: ";read DESIRED_USERNAME
 
 
-result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | docker-compose exec -T mysql mysql -uroot -p$MYSQL_ROOT_PASSWORD "$DB_NAME" | grep -c .)
+#result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | docker-compose exec -T mysql mysql -uroot -p$MYSQL_ROOT_PASSWORD "$DB_NAME" | grep -c .)
+result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | grep -c .)
 
 
 
@@ -16,7 +17,7 @@ else
   echo "User '$DESIRED_USERNAME' does not exist in the database."
 fi
 
-echo "test 3"
+echo "test 4"
 
 #result=$(docker-compose exec mysql mysql -uroot -p$MYSQL_ROOT_PASSWORD -se "SELECT COUNT(*) FROM mysql.user WHERE user = '$DESIRED_USERNAME'")
 
