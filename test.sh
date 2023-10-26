1bb42e8c3bd36f50b4911dc95f3c2423131d186f
Fallah <falahalireza11@gmail.com>
2023-10-26 20:40:37 +0330

    2 commit

--- test.sh
+++ test.sh
@@ -5,11 +5,7 @@ export $(cat .env_package | xargs)
 
 echo "name db user: ";read DESIRED_USERNAME
 
-
-#result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | docker-compose exec -T mysql mysql -uroot -p$MYSQL_ROOT_PASSWORD "$DB_NAME" | grep -c .)
-#result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | docker-compose exec -T mysql mysql -uroot -p$MYSQL_ROOT_PASSWORD | grep -c .)
-result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | docker-compose exec -T mysql mysql --defaults-file=/root/laradock/.mysql.cnf | grep -c .)
-
+result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | docker-compose exec -T mysql mysql --defaults-file=/root/laradock/.mysql.cnf  -uroot | grep -c .)
 
 if [ "$result" -gt 0 ]; then
   echo "User '$DESIRED_USERNAME' exists in the database."
@@ -17,7 +13,7 @@ else
   echo "User '$DESIRED_USERNAME' does not exist in the database."
 fi
 
-echo "test 7"
+echo "test 8"
 
-#result=$(docker-compose exec mysql mysql -uroot -p$MYSQL_ROOT_PASSWORD -se "SELECT COUNT(*) FROM mysql.user WHERE user = '$DESIRED_USERNAME'")
+#result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | docker-compose exec -T mysql mysql -uroot -p$MYSQL_ROOT_PASSWORD | grep -c .)
 
