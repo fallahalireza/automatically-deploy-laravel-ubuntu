a8f8e1748f0ce7da81717edf61beacd50a95c6d7
Fallah <falahalireza11@gmail.com>
2023-10-26 20:57:31 +0330

    2 commit

--- test.sh
+++ test.sh
@@ -5,7 +5,8 @@ export $(cat .env_package | xargs)
 
 echo "name db user: ";read DESIRED_USERNAME
 
-result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | docker-compose exec -T mysql mysql --defaults-file=/root/laradock/.mysql.cnf  -uroot | grep -c .)
+result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | docker-compose exec -T mysql mysql -uroot | grep -c .)
+
 
 if [ "$result" -gt 0 ]; then
   echo "User '$DESIRED_USERNAME' exists in the database."
@@ -13,7 +14,7 @@ else
   echo "User '$DESIRED_USERNAME' does not exist in the database."
 fi
 
-echo "test 8"
+echo "test 9"
 
 #result=$(echo "SELECT user FROM mysql.user WHERE user = '$DESIRED_USERNAME';" | docker-compose exec -T mysql mysql -uroot -p$MYSQL_ROOT_PASSWORD | grep -c .)
 
