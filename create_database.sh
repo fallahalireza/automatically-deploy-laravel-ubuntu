ea05e6dae79c2293067ee736c2f66ac9c4e24b6f
Fallah <falahalireza11@gmail.com>
2023-10-26 19:21:07 +0330

    2 commit

--- create_database.sh
+++ create_database.sh
@@ -97,7 +97,7 @@ display_gray "Have you saved your database information? (yes/no): ";read save_in
 if [ "$save_info_db" == "yes" ]; then
   cd /root/laradock
   sql_command="CREATE DATABASE IF NOT EXISTS $db_database COLLATE utf8mb4_general_ci;"
-  sql_command+="CREATE USER '$db_user'@'%' IDENTIFIED WITH mysql_native_password BY '$db_password';GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, FILE, INDEX, ALTER, CREATE TEMPORARY TABLES, CREATE VIEW, EVENT, TRIGGER, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EXECUTE ON *.* TO '$db_user'@'%';ALTER USER '$db_user'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;"
+  sql_command+="CREATE USER '$db_user'@'%' IDENTIFIED WITH mysql_native_password BY '$db_password';GRANT USAGE ON *.* TO '$db_user'@'%';ALTER USER '$db_user'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;"
   sql_command+="GRANT ALL PRIVILEGES ON $db_database.* TO '$db_user'@'%'; ALTER USER '$db_user'@'%' ;"
 
   docker-compose exec mysql mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "$sql_command"
@@ -106,23 +106,4 @@ else
    bash <(curl -Ls https://raw.githubusercontent.com/fallahalireza/automatically-deploy-laravel-ubuntu/main/install.sh)
 fi
 
-
-
-
-
-
-
-
-
-
-
-
-#-------------------------------------------
-
-# ایجاد تنظیمات محرمانگی در داخل کانتینر MySQL
-#docker-compose exec mysql mysql_config_editor set --login-path=local --host=localhost --user=root --password=root
-
-# اجرای دستور SQL برای ایجاد دیتابیس
-#docker-compose exec mysql mysql --login-path=local -e "CREATE DATABASE IF NOT EXISTS $db_database COLLATE utf8mb4_general_ci;"
-
-
+#sql_command+="CREATE USER '$db_user'@'%' IDENTIFIED WITH mysql_native_password BY '$db_password';GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, FILE, INDEX, ALTER, CREATE TEMPORARY TABLES, CREATE VIEW, EVENT, TRIGGER, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EXECUTE ON *.* TO '$db_user'@'%';ALTER USER '$db_user'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;"
