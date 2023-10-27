13a905c73a23a45321c1e30ea00f26828da82644
Fallah <falahalireza11@gmail.com>
2023-10-27 08:39:20 +0330

    2 commit

--- test.sh
+++ test.sh
@@ -1,14 +1,12 @@
-#check_database_existence() {
-#  local database_name="$1"
-#  local MYSQL_ROOT_PASSWORD="$2"
-#  local sql_query="SHOW DATABASES LIKE '$database_name';"
-#  local result=$(docker-compose exec mysql mysql -uroot -p$MYSQL_ROOT_PASSWORD -se "$sql_query")
-#
-#  if [ "$result" = "$database_name" ]; then
-#    echo "Database '$database_name' already exists."
-#    return 0
-#  else
-#    echo "Database '$database_name' does not exist."
-#    return 1
-#  fi
-#}
+# اجرای دستور ifconfig برای دریافت آدرس IPv4
+ipv4_address=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+
+# نمایش آدرس IPv4
+echo "آدرس IPv4 سرور: $ipv4_address"
+
+
+# اجرای دستور ip برای دریافت آدرس IPv4
+ipv4_address2=$(ip addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# نمایش آدرس IPv4
+echo "آدرس IPv4 سرور: $ipv4_address2"
