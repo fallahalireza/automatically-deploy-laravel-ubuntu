9ed4f273f849ef62fd3ff7f5435518542b5ef2d1
Fallah <falahalireza11@gmail.com>
2023-10-28 22:24:42 +0330

    2 commit

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "test 2"
+echo "test 3"
 echo "Enter email: ";read ssl_email
 echo "Enter the domain: ";read ssl_domain
 cd /root/laradock || exit
 docker-compose exec nginx apk add certbot certbot-nginx
-docker-compose exec nginx certbot certonly --email $ssl_email --no-eff-email -d $ssl_domain -d www.$ssl_domain
+echo "1" | docker-compose exec nginx certbot certonly --email $ssl_email --no-eff-email -d $ssl_domain -d www.$ssl_domain
+
 
