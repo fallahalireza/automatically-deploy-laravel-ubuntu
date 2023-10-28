e9a6c8858e711515752044e6884372ad8946dbee
Fallah <falahalireza11@gmail.com>
2023-10-28 22:05:55 +0330

    2 commit

--- test.sh
+++ test.sh
@@ -1,12 +1,7 @@
-# اجرای دستور ifconfig برای دریافت آدرس IPv4
-ipv4_address=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+#!/bin/bash
 
-# نمایش آدرس IPv4
-echo "آدرس IPv4 سرور: $ipv4_address"
+display_gray "Enter email: ";read ssl_email
+display_gray "Enter the domain: ";read ssl_domain
+docker-compose exec nginx apk add certbot certbot-nginx
+docker-compose exec nginx certbot certonly --email $ssl_email --no-eff-email -d $ssl_domain -d www.$ssl_domain
 
-
-# اجرای دستور ip برای دریافت آدرس IPv4
-ipv4_address2=$(ip addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-
-# نمایش آدرس IPv4
-echo "آدرس IPv4 سرور: $ipv4_address2"
