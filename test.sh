9af4d3a048887f47629012706af87d3faeabe6fc
Fallah <falahalireza11@gmail.com>
2023-10-28 22:08:02 +0330

    2 commit

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-display_gray "Enter email: ";read ssl_email
-display_gray "Enter the domain: ";read ssl_domain
+echo "Enter email: ";read ssl_email
+echo "Enter the domain: ";read ssl_domain
 docker-compose exec nginx apk add certbot certbot-nginx
 docker-compose exec nginx certbot certonly --email $ssl_email --no-eff-email -d $ssl_domain -d www.$ssl_domain
 
